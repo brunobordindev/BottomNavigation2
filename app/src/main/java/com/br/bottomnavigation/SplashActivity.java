cb307525db41ac3c25f9d069f73bf0068347aec9
Bruno Rodrigues Bordin <bbordin@server01.foursys.local>
2021-06-01 21:29:42 -0300

    update: subindo arquivo

---
+++ app/src/main/java/com/br/bottomnavigation/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.br.bottomnavigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getBaseContext(), MainActivity.class));
+            }
+        }, 3000);
+    }
+}
